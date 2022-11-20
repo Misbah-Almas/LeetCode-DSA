2db1d6f0dd95b9fb20f4c1445ce30eecbd13fb52
Misbah Almas <dev.misbahalmas@gmail.com>
2022-11-20 18:59:41 +0530

    Added solution - LeetHub

---
+++ Max distance between same elements - GFG/max-distance-between-same-elements.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			Solution ob = new Solution();
+			System.out.println(ob.maxDistance(arr,n));
+		
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+// your task is to complete this function
+class Solution
+{
+    int maxDistance(int arr[], int n)
+    {
+	// Your code here
+	int length=0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            if(map.containsKey(arr[i])){
+                length = Math.max(length, i-map.get(arr[i]));
+            }
+            else
+                map.put(arr[i], i);
+        }
+        return length;
+    }
+}
