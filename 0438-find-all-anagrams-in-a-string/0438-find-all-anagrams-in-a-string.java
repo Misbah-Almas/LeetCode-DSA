cdc5b55ef79c7968433cdef317c5d9b870c14d01
Misbah Almas <dev.misbahalmas@gmail.com>
2022-11-20 13:05:36 +0530

    Time: 21 ms (69.29%), Space: 47.8 MB (52.19%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,43 +1,34 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-    int n = s.length();
-    int m = p.length();
-    
-    int sfreq[] = new int[26];
-    int pfreq[] = new int[26];
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = s.length();
+        int m = p.length();
+        int[] sfreq = new int[26];
+        int[] pfreq = new int[26];
         
-    List<Integer> ans = new ArrayList<>();
+
+        for(int i=0; i<m; i++){
+            pfreq[p.charAt(i)-'a']++;
+        }
         
-    if(m>n)
-        return ans;
-    
-    // build your pfreq map
-    for(int i = 0; i < m; i++) {
-        pfreq[p.charAt(i) - 'a']++;
-    }
-    
-    int start = 0;
-    int end = 0;
-    
+        int start=0;
+        int end=0;
         
-    while(end < n)
-    {
-        sfreq[s.charAt(end) - 'a']++;
-        
-        // reached the box -> length condition satisfied
-        // compare maps, decrease start freq and move start
-        if(end - start + 1 == m) 
-        {
-            if(Arrays.equals(sfreq, pfreq)) ans.add(start);
-            sfreq[s.charAt(start) - 'a']--;
+        while(end<n){
+            sfreq[s.charAt(end)-'a']++;
+            
+        if(end-start+1==m){
+            if(Arrays.equals(sfreq, pfreq)){
+                arr.add(start);
+            }
+            
+          sfreq[s.charAt(start)-'a']--;
             start++;
         }
-        end++;
-    }
-
-    return ans;
-
+            end++;
+        }
         
+        return arr;
     }
 }
