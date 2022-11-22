fc781dcfd404fcde09caeb94a50534f7f06eea62
Misbah Almas <dev.misbahalmas@gmail.com>
2022-11-22 23:11:35 +0530

    Time: 2 ms (85.47%), Space: 51.7 MB (24.29%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
-        for(int i: nums){ // xor with whole array
+        for(int i: nums){ // xor ans with whole array
             ans ^= i;
         }
         
