a0e3c97ca88e1c8e1787c209600cb7280bd049f5
Misbah Almas <dev.misbahalmas@gmail.com>
2022-11-24 23:59:13 +0530

    Time: 2 ms (89.63%), Space: 46.3 MB (91.42%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] countBits(int n) {
+    public int[] countBits(int n) { // Using Bit Manipulation
         int[] ans = new int[n+1];
         for(int i=0; i<=n; i++){
             ans[i] = i%2==1 ? ans[i/2]+1 : ans[i/2];
