ca64e4ebd27510d8bf445305003cd871ed4a8704
Misbah Almas <dev.misbahalmas@gmail.com>
2022-12-11 23:36:40 +0530

    Time: 1 ms (79.15%), Space: 41.5 MB (63.07%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        int left=0; int right=0;
+		if(root==null || (root.left==null && root.right==null))
+			return true;
+		if(root.left != null) left = root.left.val;
+		if(root.right != null) right = root.right.val;
+		if((root.val == left + right) && (checkTree(root.left) && checkTree(root.right)))
+			return true;
+		return false;
+    }
+}
