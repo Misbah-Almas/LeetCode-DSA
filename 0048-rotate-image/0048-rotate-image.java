2d1fd28524c44849522081a1305717d08bbfbefb
Misbah Almas <dev.misbahalmas@gmail.com>
2022-12-15 20:05:16 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (82.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,8 +1,8 @@
 class Solution {
     public void rotate(int[][] a) {
-        int n  = a.length;
-        for(int i = 0 ;i<n/2;i++){
-            for(int j = i; j<n-1-i;j++){
+        int n = a[0].length;
+        for(int i=0; i<n/2; i++){
+            for(int j=i; j<n-1-i; j++){
                 int temp = a[i][j];
                 a[i][j] = a[n-1-j][i];
                 a[n-1-j][i] = a[n-1-i][n-1-j];
@@ -10,5 +10,31 @@ class Solution {
                 a[j][n-1-i] = temp;
             }
         }
-    }
+    }    
 }
+// Brute force approach will give TLE
+// class Solution {
+//     public void rotate(int[][] a) {
+//         int n = a[0].length;
+//         for(int i=0; i<n; i++){
+//             for(int j=i; j<n; j++){
+//                 int temp = a[i][j];
+//                 a[i][j] = a[j][i];
+//                 a[j][i] = a[i][j];
+//             }
+//         }
+//         for(int i=0; i<n; i++){
+//             reverse(a[i]);
+//         }
+//     }
+    
+//     private void reverse(int[] arr){
+//         int i=0;
+//         int j=arr.length-1;
+//         while(i<j){
+//             int temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+// }
