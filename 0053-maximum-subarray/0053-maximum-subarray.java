464603c3c2d20a201228ec0a4a72d834ebfbc52c
Misbah Almas <dev.misbahalmas@gmail.com>
2023-01-23 14:21:55 +0530

    Time: 1 ms (100.00%), Space: 51.5 MB (85.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        // Kadane’s Algorithm
         int sum = 0;
         int max = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++){
