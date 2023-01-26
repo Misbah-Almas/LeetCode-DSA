2657e193740530caddc0588cc87751b1ffea2ccc
Misbah Almas <dev.misbahalmas@gmail.com>
2023-01-26 23:19:33 +0530

    Time: 0 ms (100.00%), Space: 40 MB (98.61%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,15 +14,43 @@
  * }
  */
 class Solution {
-    private void preOrder(TreeNode root, List<Integer> ans){
-        if(root==null) return;
-        ans.add(root.val);
-        preOrder(root.left, ans);
-        preOrder(root.right, ans);
-    }
+    // private void preOrder(TreeNode root, List<Integer> ans){
+    //     if(root==null) return;
+    //     ans.add(root.val);
+    //     preOrder(root.left, ans);
+    //     preOrder(root.right, ans);
+    // }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preOrder(root, ans);
+        // preOrder(root, ans);
+        // return ans;
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left==null){
+            ans.add(curr.val);
+            curr = curr.right;
+        }
+        else{
+            TreeNode leftNode = curr.left;
+            TreeNode rightMostNode = rightMost(leftNode, curr.val);
+            if(rightMostNode.right==null){
+                ans.add(curr.val);
+                rightMostNode.right = curr;
+                curr = curr.left;
+            }
+            else{
+                rightMostNode.right=null;
+                curr = curr.right;
+            }
+        }
+      }
         return ans;
+    }
+    
+    private TreeNode rightMost(TreeNode root, int k){
+        while(root.right != null && root.right.val!= k){
+            root = root.right;
+        }
+        return root;
     }
 }
