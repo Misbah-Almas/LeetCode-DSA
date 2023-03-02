d4a7a96259aa83837c39890c590349134494ccfd
Misbah Almas <dev.misbahalmas@gmail.com>
2023-03-02 15:48:43 +0530

    Time: 34 ms (62.22%), Space: 54 MB (39.00%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,37 +1,43 @@
-public class Solution {
-    public void mergeSort(int[] nums, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        int mid = (left + right) / 2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-        merge(nums, left, mid, right);
-    }
-
-    public void merge(int[] nums, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) {
-                temp[k++] = nums[i++];
-            } else {
-                temp[k++] = nums[j++];
-            }
-        }
-        while (i <= mid) {
-            temp[k++] = nums[i++];
-        }
-        while (j <= right) {
-            temp[k++] = nums[j++];
-        }
-        for (int p = 0; p < k; p++) {
-            nums[left + p] = temp[p];
-        }
-    }
-
+class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
+        mergeSort(nums, 0, nums.length-1);
         return nums;
     }
+    
+    public void mergeSort(int[] nums, int left, int right){
+        if(left<right){
+            int mid = (left + right)/2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid+1, right);
+            merge(nums, left, mid, right);
+        }
+        return;
+    }
+    
+    public void merge(int arr[], int left, int mid, int right){
+        int i = left;
+        int j = mid+1;
+        int[] temp = new int[right-left+1];
+        int k = 0;
+        while(i<=mid && j<=right){
+            if(arr[i]<=arr[j]){
+                temp[k++] = arr[i++];
+            }
+            else{
+                temp[k++] = arr[j++];
+            }
+        }
+        
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+        while(j<=right){
+            temp[k++] = arr[j++];
+        }
+        
+        for(int z=0; z<k; z++){
+            arr[left+z] = temp[z];
+        }
+    }
+    
 }
